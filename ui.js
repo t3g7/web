397b728e90a54a0cefcf295aa2ecea20ac779837
eftov <b.fovet@gmail.com>
2015-11-29 13:18:06 +0100

    Convert tabs to spaces

--- ui.js
+++ ui.js
@@ -9,9 +9,9 @@ app.use(express.static(__dirname + '/public'));
 var api = "http://localhost:8080";
 
 app.get('/', function(req, res) {
-	request(api + '/tweets', function(err, result) {
-		res.sendfile("./public/index.html");
-	});
+  request(api + '/tweets', function(err, result) {
+    res.sendfile("./public/index.html");
+  });
 });
 
 app.listen(8081);
