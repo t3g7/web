adfc53d7435d164e88e4f112563f8f481aa53631
eftov <b.fovet@gmail.com>
2016-01-17 23:01:47 +0100

    Replace chart with stacked barchart + automate build

--- public/charts.js
+++ public/charts.js
@@ -1,203 +1,225 @@
-var margin = {
-        top: 20,
-        right: 20,
-        bottom: 30,
-        left: 40
-    },
-    width = 900 - margin.left - margin.right,
-    height = 250 - margin.top - margin.bottom;
-
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-var y = d3.scale.linear().range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-
-var svg = d3.select(".tweets-freq-graph").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var w = 825;
+var h = 260;
+var padding = {top: 20, right: 40, bottom: 30, left: 20};
 
 var formatDateUtc = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
 
 d3.json('data.json', function(error, data) {
-    //console.log(data);
+  var color_hash = {
+		0: ["Neutral","#ffff00"],
+		1: ["Positive","#00FF00"],
+		2: ["Negative","#ff0000"]
+	};
 
-    var countPos = {};
-    var countNeg = {};
-    var countNeu = {};
-    data.forEach(function(d) {
-      var datetime = formatDateUtc.parse(d.created_at);
-      var date = formatDateUtc(datetime).split('T')[0]
-          + ' '
-          + formatDateUtc(datetime).split('T')[1].split(':')[0]
-          + ':' + formatDateUtc(datetime).split('T')[1].split(':')[1];
+  var countPos = {};
+  var countNeg = {};
+  var countNeu = {};
+  data.forEach(function(d) {
+    var datetime = formatDateUtc.parse(d.created_at);
+    var date = formatDateUtc(datetime).split('T')[0]
+        + ' '
+        + formatDateUtc(datetime).split('T')[1].split(':')[0]
+        + ':' + formatDateUtc(datetime).split('T')[1].split(':')[1];
 
-      countNeg[date] = 0;
-      countPos[date] = 0;
-      countNeu[date] = 0;
-    });
+    countNeg[date] = 0;
+    countPos[date] = 0;
+    countNeu[date] = 0;
+  });
 
-    var count = {};
+  var count = {};
+  var sentimentDict = {};
+  data.forEach(function(d) {
+    //var t = d.created_at.split(/[-T:]/);
+    //var date = new Date(t[0], t[1], t[2], t[3], t[4]);
 
-    var countSentiment = {};
-    var sentimentDict = {};
-    //var count2 = [];
-    data.forEach(function(d) {
-        //var t = d.created_at.split(/[-T:]/);
-        //var date = new Date(t[0], t[1], t[2], t[3], t[4]);
-
-        var datetime = formatDateUtc.parse(d.created_at);
-        var date = formatDateUtc(datetime).split('T')[0]
-            + ' '
-            + formatDateUtc(datetime).split('T')[1].split(':')[0]
-            + ':' + formatDateUtc(datetime).split('T')[1].split(':')[1];
+    var datetime = formatDateUtc.parse(d.created_at);
+    var date = formatDateUtc(datetime).split('T')[0]
+        + ' '
+        + formatDateUtc(datetime).split('T')[1].split(':')[0]
+        + ':' + formatDateUtc(datetime).split('T')[1].split(':')[1];
         //+ ':' + formatDateUtc(datetime).split('T')[1].split(':')[2];
-        //count2.push(date);
-        var sentiment = d.sentiment;
+    count[date] = (count[date]||0) + 1;
 
-        countSentiment[sentiment+date] = (count[date]||0) + 1;
-
-        if (countSentiment)
-
-        sentimentDict[date] = countSentiment
-
-        /*var sentimentList = [];
-        if (date in sentimentDict) {
-          sentimentDict[date].push(sentiment);
-        } else {
-          sentimentList.push(sentiment);
-          sentimentDict[date] = sentimentList;
-        }*/
-
-
-        if (sentiment === "POSITIVE") {
-          countPos[date] = (countPos[date]||0) + 1;
-
-        } else if (sentiment === "NEGATIVE") {
-          countNeg[date] = (countNeg[date]||0) + 1;
-        } else if (sentiment === "NEUTRAL") {
-          countNeu[date] = (countNeu[date]||0) + 1;
-        }
-
-
-        count[date] = (count[date]||0) + 1;
-    });
-
-    console.log(count);
-    console.log("countPOS: ");
-    console.log(countPos);
-    console.log("countNEG: ");
-    console.log(countNeg);
-    console.log("countNEU: ");
-    console.log(countNeu);
-    //console.log(countSentiment);
-    //console.log(sentimentDict);
-    /*
-     count2 = count2.sort(function(a, b) {
-     return new Date(a) - new Date(b);
-     });
-
-     var minDate = new Date(count2[0]).getTime();
-     var maxDate = new Date(count2[count2.length - 1]).getTime();
-
-     var count3 = [];
-     var currentDate = minDate;
-     var d;
-
-     while (currentDate <= maxDate) {
-     d = new Date(currentDate);
-     count3.push(d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes());
-     currentDate += (60 * 1000);
-     }*/
-
-    var freq_data = [];
-    for (var date in count) {
-        if (count.hasOwnProperty(date)) {
-            freq_data.push({
-                date: new Date(date),
-                frequency: count[date],
-                sentiment: sentimentDict[date]
-            })
-        }
+    var sentiment = d.sentiment;
+    if (sentiment === "POSITIVE") {
+      countPos[date] = (countPos[date]||0) + 1;
+    } else if (sentiment === "NEGATIVE") {
+      countNeg[date] = (countNeg[date]||0) + 1;
+    } else if (sentiment === "NEUTRAL") {
+      countNeu[date] = (countNeu[date]||0) + 1;
     }
 
-    freq_data = freq_data.sort(function(a, b) {
-        return new Date(a.date) - new Date(b.date);
-    });
 
-    console.log(freq_data);
+  });
 
-    var tweet_texts = [];
-    data.forEach(function(d) {
-        var body = d.body;
-        var retweets = d.retweet_count;
-        var favs = d.favorite_count;
+  var dataset = [
+    d3.entries(countNeu).sort(function(a, b) {
+      return new Date(a.key) - new Date(b.key);
+    }),
+    d3.entries(countPos).sort(function(a, b) {
+      return new Date(a.key) - new Date(b.key);
+    }),
+    d3.entries(countNeg).sort(function(a, b) {
+      return new Date(a.key) - new Date(b.key);
+    })
+  ];
 
-        if (tweet_texts.indexOf(body) == -1) {
-            tweet_texts.push({
-                body: body,
-                retweets: retweets,
-                favs: favs
-            });
-        }
-    });
+  d3.layout.stack(dataset);
 
-    d3.select('.tweets-log').append('ul').selectAll('li')
-        .data(tweet_texts)
-        .enter()
-        .append('li')
-        .html(function(d) {
-            return d.body + " " + d.favs + " " + d.retweets;
-        });
+  //Set up scales
+	var xScale = d3.time.scale()
+		  .domain([new Date(dataset[0][0].key), d3.time.minute.offset(new Date(dataset[0][dataset[0].length - 1].key), 8)])
+		  .rangeRound([0, w - padding.left - padding.right]);
 
-    x.domain(freq_data.map(function (d) {
-        return d.date;
-    }));
-    y.domain([0, d3.max(freq_data, function (d) {
-        return d.frequency;
-    })]);
+	var yScale = d3.scale.linear()
+          		 .domain([0,
+          		    d3.max(dataset, function(d) {
+          				  return d3.max(d, function(d) {
+          					  return d.value;
+          				  });
+          			  })
+          		 ])
+        		   .range([h - padding.bottom - padding.top, 0]);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+	var xAxis = d3.svg.axis()
+				      .scale(xScale)
+				      .orient("bottom")
+				      .ticks(d3.time.minute, 2);
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
+	var yAxis = d3.svg.axis()
+				      .scale(yScale)
+				      .orient("left")
+              .tickFormat(d3.format("d"))
+				      .ticks(10);
 
-    svg.selectAll(".bar")
-        .data(freq_data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("fill", "teal")
-        .attr("x", function (d) {
-            return x(d.date);
-        })
-        .attr("width", x.rangeBand())
-        .attr("y", function (d) {
-            return y(d.frequency);
-        })
-        .attr("height", function (d) {
-            return height - y(d.frequency);
-        });
+	//Create SVG element
+	var svg = d3.select("#bar")
+    				.append("svg")
+    				.attr("width", w)
+    				.attr("height", h);
 
-    function type(d) {
-        d.frequency = +d.frequency;
-        return d;
-    }
+	// Add a group for each row of data
+	var groups = svg.selectAll("g")
+		.data(dataset)
+		.enter()
+		.append("g")
+		.attr("class", "rgroups")
+		.attr("transform", "translate("+ padding.left + "," + (h - padding.bottom) +")")
+		.style("fill", function(d, i) {
+			return color_hash[dataset.indexOf(d)][1];
+		});
+
+	// Add a rect for each data value
+	var rects = groups.selectAll("rect")
+		.data(function(d) { return d; })
+		.enter()
+		.append("rect")
+		.attr("width", 2)
+		.style("fill-opacity", 1e-6);
+
+	rects.transition()
+	      .duration(function(d, i) {
+	    	  return 5 * i;
+	      })
+	      .ease("linear")
+	      .attr("x", function(d) {
+			    return xScale(new Date(d.key));
+    		})
+    		.attr("y", function(d) {
+    			return -( - yScale(d.value) - 210 + (h - padding.top - padding.bottom) * 2);
+    		})
+    		.attr("height", function(d) {
+    			return - yScale(d.value) + (h - padding.top - padding.bottom);
+    		})
+    		.attr("width", 15)
+    		.style("fill-opacity", 1);
+
+	svg.append("g")
+		.attr("class","x axis")
+		.attr("transform","translate(20," + (h - padding.bottom) + ")")
+		.call(xAxis);
+
+	svg.append("g")
+		.attr("class","y axis")
+		.attr("transform","translate(" + padding.left + "," + padding.top + ")")
+		.call(yAxis);
+
+	// Add legend
+	var legend = svg.append("g")
+					.attr("class","legend")
+					.attr("x", w - padding.right - 65)
+					.attr("y", 25)
+					.attr("height", 100)
+					.attr("width", 100);
+
+	legend.selectAll("g").data(dataset)
+		  .enter()
+		  .append('g')
+		  .each(function(d,i){
+		  	var g = d3.select(this);
+		  	g.append("rect")
+		  		.attr("x", w - padding.right - 65)
+		  		.attr("y", i*25 + 10)
+		  		.attr("width", 10)
+		  		.attr("height",10)
+		  		.style("fill",color_hash[String(i)][1]);
+
+		  	g.append("text")
+		  	 .attr("x", w - padding.right - 50)
+		  	 .attr("y", i*25 + 20)
+		  	 .attr("height",30)
+		  	 .attr("width",100)
+		  	 .style("fill",color_hash[String(i)][1])
+		  	 .text(color_hash[String(i)][0]);
+		  });
+
+  var freq_data = [];
+  for (var date in count) {
+      if (count.hasOwnProperty(date)) {
+          freq_data.push({
+              date: new Date(date),
+              frequency: count[date],
+              sentiment: sentimentDict[date]
+          })
+      }
+  }
+
+  freq_data = freq_data.sort(function(a, b) {
+      return new Date(a.date) - new Date(b.date);
+  });
+
+  var tweet_texts = [];
+  data.forEach(function(d) {
+      var body = d.body;
+      var retweets = d.retweet_count;
+      var sentiment = d.sentiment;
+
+      if (tweet_texts.indexOf(body) == -1) {
+          tweet_texts.push({
+              body: body,
+              retweets: retweets,
+              sentiment: sentiment
+          });
+      }
+  });
+
+  d3.select('.tweets-log').append('ul').selectAll('li')
+      .data(tweet_texts)
+      .enter()
+      .append('li')
+      .html(function(d) {
+          return d.body + " " + d.sentiment  + " " + d.retweets;
+      });
+
+  x.domain(freq_data.map(function (d) {
+      return d.date;
+  }));
+  y.domain([0, d3.max(freq_data, function (d) {
+      return d.frequency;
+  })]);
+
+  function type(d) {
+      d.frequency = +d.frequency;
+      return d;
+  }
 });
